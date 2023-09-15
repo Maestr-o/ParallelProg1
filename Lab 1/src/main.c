ee33d426de5847b1ee79da3e2c3fcd2492d9d34b
Evgeniy <evg54@internet.ru>
2023-09-15 11:54:56 +0300

    Mult works without sum

--- Lab 1/src/main.c
+++ Lab 1/src/main.c
@@ -1,36 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void print_pol(int *p, int k);
-int *fill_pol(FILE *fp, int *k);
+typedef struct {
+    int coefficient;
+    int *exponents;
+} term_t;
+
+void print_pol(term_t pol[], int n, int k);
+int *fill_array(FILE *fp, int *k);
+void mult_pol(int n, term_t pol1[], int size1, term_t pol2[], int size2, term_t result[]);
+void fill_struct(term_t *s, int *p, int n, int k);
 
 int main() {
     char path[256];
     printf("Enter the path to file: ");
-    scanf("%255s", path);
+    //scanf("%255s", path);
+    strcpy(path, "../datasets/1.txt");
     FILE *fp = fopen(path, "r");
     if (!fp) {
         printf("Can't open the file");
         return 0;
     }
-    int k1 = 0, k2 = 0;
-    int *p1 = fill_pol(fp, &k1);
-    int *p2 = fill_pol(fp, &k2);
-    fclose(fp);
-    print_pol(p1, k1);
-    print_pol(p2, k2);
-    free(p1);
-    free(p2);
+    int n = 0, k1 = 0, k2 = 0;
+    char c = getc(fp);
+    while (c != '\n') {
+        n = n * 10 + c - '0';
+        c = getc(fp);
+    }
+    int *p1 = fill_array(fp, &k1);
+    int *p2 = fill_array(fp, &k2);
+    k1 /= (n + 1);
+    k2 /= (n + 1);
+    term_t pol1[k1], pol2[k2], res[k1 + k2];
+    fill_struct(pol1, p1, n, k1);
+    fill_struct(pol2, p2, n, k2);
+    mult_pol(n, pol1, k1, pol2, k2, res);
+
+    for (int i = 0; i < k1 * k2; i++) { // вывод
+        printf("(%d, ", res[i].coefficient);
+        for (int j = 0; j < n - 1; j++) {
+            printf("%d, ", res[i].exponents[j]);
+        }
+        printf("%d); ", res[i].exponents[n - 1]);
+    }
+    printf("\n");
+    //free
     return 0;
 }
 
-void print_pol(int *p, int k) {
-    for (int i = 0; i < k; i++)
-        printf("%d ", p[i]);
-    printf("\n");
-}
-
-int *fill_pol(FILE *fp, int *k) {
+int *fill_array(FILE *fp, int *k) {
     int *p = (int *)malloc(sizeof(int));
     int i = 0;
     char c = getc(fp);
@@ -52,4 +71,29 @@ int *fill_pol(FILE *fp, int *k) {
     }
     *k = i;
     return p;
+}
+
+void mult_pol(int n, term_t pol1[], int size1, term_t pol2[], int size2, term_t result[]) {
+    int i, j, k;
+    int size_result = 0;
+    for (i = 0; i < size1; i++) {
+        for (j = 0; j < size2; j++) {
+            result[size_result].coefficient = pol1[i].coefficient * pol2[j].coefficient;
+            result[size_result].exponents = (int *)malloc(sizeof(int) * n);
+            for (k = 0; k < n; k++) {
+                result[size_result].exponents[k] = pol1[i].exponents[k] + pol2[j].exponents[k];
+            }
+            size_result++;
+        }
+    }
+}
+
+void fill_struct(term_t *s, int *p, int n, int k) {
+    for (int i = 0; i < k; i++) {
+        s[i].exponents = (int *)malloc(sizeof(int) * n);
+        s[i].coefficient = p[i * (n + 1)];
+        for (int j = 0; j < n; j++) {
+            s[i].exponents[j] = p[i * (n + 1) + j + 1];
+        }
+    }
 }
