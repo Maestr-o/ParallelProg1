4917c1c071e45c3ae6f6ef8105c5a7d707c627f7
Evgeniy <evg54@internet.ru>
2023-09-16 16:22:26 +0300

    Added parallel calc, output to files, datasets

--- Lab 1/src/main.c
+++ Lab 1/src/main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <omp.h>
 
-typedef struct {
-    int coefficient;
-    int *exponents;
+#define SEQ_OUT_PATH "../build/sout.txt"
+#define PAR_OUT_PATH "../build/pout.txt"
+
+typedef struct { // структура монома
+    long coefficient;
+    long *exponents;
 } mon_t;
 
-int *fill_array(FILE *fp, int *k);
-void mult_pol(int n, mon_t pol1[], int size1, mon_t pol2[], int size2, mon_t res[]);
-void fill_struct(mon_t *s, int *p, int n, int k);
-void print(mon_t res[], int n, int k);
-void free_pol(mon_t res[], int k);
-int check_file(FILE *fp);
+long *fill_array(FILE *fp, int *k); // заполнение массива-полинома из файла
+void mult_pol(int n, mon_t pol1[], int size1, mon_t pol2[], int size2, mon_t res[]); // последовательное перемножение
+void mult_pol_parallel(int n, mon_t pol1[], int k1, mon_t pol2[], int k2, mon_t res[]); // параллельное перемножение
+void fill_struct(mon_t *s, long *p, int n, int k); // заполнение массива структур
+void print(mon_t res[], int n, int k, char *path); // вывод результата в файл
+void free_pol(mon_t res[], int k); // освобождение памяти массива структур
+int check_file(FILE *fp); //проверка на существование файла
 void run();
 
 int main() {
@@ -31,25 +37,35 @@ void run() {
         n = n * 10 + c - '0';
         c = getc(fp);
     }
-    int *p1 = fill_array(fp, &k1);
-    int *p2 = fill_array(fp, &k2);
+    long *p1 = fill_array(fp, &k1);
+    long *p2 = fill_array(fp, &k2);
     fclose(fp);
     k1 /= (n + 1);
     k2 /= (n + 1);
-    mon_t pol1[k1], pol2[k2], res[k1 * k2];
+    mon_t pol1[k1], pol2[k2], res[k1 * k2], res_par[k1 * k2];
     fill_struct(pol1, p1, n, k1);
     fill_struct(pol2, p2, n, k2);
+    double start, end;
+    start = omp_get_wtime();
     mult_pol(n, pol1, k1, pol2, k2, res);
-    //очистка
+    end = omp_get_wtime();
+    printf("Seq: %.3lf sec\n", (end - start));
+    start = omp_get_wtime();
+    mult_pol_parallel(n, pol1, k1, pol2, k2, res_par);
+    end = omp_get_wtime();
+    printf("Parallel: %.3lf sec\n", (end - start));
+    print(res, n, k1 * k2, SEQ_OUT_PATH);
+    print(res, n, k1 * k2, PAR_OUT_PATH);
     free(p1);
     free(p2);
     free_pol(pol1, k1);
     free_pol(pol2, k2);
     free_pol(res, k1 * k2);
+    free_pol(res_par, k1 * k2);
 }
 
-int *fill_array(FILE *fp, int *k) {
-    int *p = (int *)malloc(sizeof(int));
+long *fill_array(FILE *fp, int *k) {
+    long *p = (long *)malloc(sizeof(long));
     int i = 0;
     char c = getc(fp);
     while (c != EOF && c != '\n') {
@@ -59,7 +75,7 @@ int *fill_array(FILE *fp, int *k) {
             c = getc(fp);
         }
         int num = c - '0';
-        p = (int *)realloc(p, sizeof(int) * (i + 2));
+        p = (long *)realloc(p, sizeof(long) * (i + 2));
         c = getc(fp);
         while (c >= '0' && c <= '9' && c != '\n' && c != EOF) {
             num = num * 10 + c - '0';
@@ -77,7 +93,7 @@ void mult_pol(int n, mon_t pol1[], int k1, mon_t pol2[], int k2, mon_t res[]) {
     for (int i = 0; i < k1; i++) {
         for (int j = 0; j < k2; j++) {
             res[size_result].coefficient = pol1[i].coefficient * pol2[j].coefficient;
-            res[size_result].exponents = (int *)calloc(sizeof(int), n);
+            res[size_result].exponents = (long *)calloc(sizeof(long), n);
             for (int k = 0; k < n; k++) {
                 res[size_result].exponents[k] = pol1[i].exponents[k] + pol2[j].exponents[k];
             }
@@ -100,9 +116,40 @@ void mult_pol(int n, mon_t pol1[], int k1, mon_t pol2[], int k2, mon_t res[]) {
     }
 }
 
-void fill_struct(mon_t *s, int *p, int n, int k) {
+void mult_pol_parallel(int n, mon_t pol1[], int k1, mon_t pol2[], int k2, mon_t res[]) {
+    #pragma omp parallel for
+    for (int i = 0; i < k1; i++) {
+        for (int j = 0; j < k2; j++) {
+            int local_size_result = i * k2 + j;
+            res[local_size_result].coefficient = pol1[i].coefficient * pol2[j].coefficient;
+            res[local_size_result].exponents = (long *)calloc(sizeof(long), n);
+            for (int k = 0; k < n; k++) {
+                res[local_size_result].exponents[k] = pol1[i].exponents[k] + pol2[j].exponents[k];
+            }
+        }
+    }
+    int size_result = 0;
+    #pragma omp parallel for reduction(+:size_result)
+    for (int i = 0; i < k1 * k2 - 1; i++) { // подсчет подобных
+        for (int j = i + 1; j < k1 * k2 && res[i].coefficient != 0; j++) {
+            if (res[j].coefficient != 0) {
+                if (res[i].exponents[0] == res[j].exponents[0] &&
+                    res[i].exponents[1] == res[j].exponents[1] &&
+                    res[i].exponents[2] == res[j].exponents[2]) {
+                    #pragma omp atomic
+                    res[i].coefficient += res[j].coefficient;
+                    res[j].coefficient = 0;
+                    #pragma omp atomic
+                    size_result++;
+                }
+            }
+        }
+    }
+}
+
+void fill_struct(mon_t *s, long *p, int n, int k) {
     for (int i = 0; i < k; i++) {
-        s[i].exponents = (int *)malloc(sizeof(int) * n);
+        s[i].exponents = (long *)malloc(sizeof(long) * n);
         s[i].coefficient = p[i * (n + 1)];
         for (int j = 0; j < n; j++) {
             s[i].exponents[j] = p[i * (n + 1) + j + 1];
@@ -110,17 +157,19 @@ void fill_struct(mon_t *s, int *p, int n, int k) {
     }
 }
 
-void print(mon_t res[], int n, int k) {
+void print(mon_t res[], int n, int k, char *path) {
+    FILE *fp = fopen(path, "w");
+    if (!check_file(fp)) return;
     for (int i = 0; i < k; i++) {
         if (res[i].coefficient != 0) {
-            printf("(%d, ", res[i].coefficient);
+            fprintf(fp, "(%ld, ", res[i].coefficient);
             for (int j = 0; j < n - 1; j++) {
-                printf("%d, ", res[i].exponents[j]);
+                fprintf(fp, "%ld, ", res[i].exponents[j]);
             }
-            printf("%d); ", res[i].exponents[n - 1]);
+            fprintf(fp, "%ld); ", res[i].exponents[n - 1]);
         }
     }
-    printf("\n");
+    fclose(fp);
 }
 
 void free_pol(mon_t res[], int k) {
